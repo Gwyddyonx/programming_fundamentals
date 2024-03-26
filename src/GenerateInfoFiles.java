0bfdb7d22707623d336e2e21c11c4b5b9ba65f63
Camilo Montero <cmontero@sproutloud.com>
2024-03-26 13:38:32 -0500

    adding createProductsFile and createSalesManInfoFile implementation

--- src/GenerateInfoFiles.java
+++ src/GenerateInfoFiles.java
@@ -7,6 +7,18 @@ public class GenerateInfoFiles {
     private static final int NUMBER_OF_PRODUCTS = 50;
     private static final int NUMBER_OF_SALES_MAN = 50;
 
+    // array with 20 products
+    private static final String[] PRODUCTS = {
+            "T-shirt", "Jeans", "Shoes", "Scarf", "Hat", "Sunglasses", "Watch", "Bag", "Gloves", "Dress",
+            "Sweater", "Skirt", "Jacket", "Boots", "Socks", "Belt", "Suit", "Blouse", "Tie", "Coat"
+    };
+
+    // Arrays with names and surnames
+    private static final String[] NAMES = { "John", "Emily", "Michael", "Jessica", "David", "Sarah", "Daniel",
+            "Jennifer", "James", "Emma" };
+    private static final String[] SURNAMES = { "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller",
+            "Wilson", "Moore", "Taylor" };
+
     // for random numbers
     private static final int MIN_NUMBER = 1;
 
@@ -19,13 +31,15 @@ public class GenerateInfoFiles {
             createSalesMenFile(NUMBER_OF_SALES, vendor_name, id_name);
 
             // createProductsFile
+            createProductsFile(NUMBER_OF_PRODUCTS);
 
             // createSalesManInfoFile
+            createSalesManInfoFile(NUMBER_OF_SALES_MAN);
 
-            System.err.println("Archivos generados correctamente");
+            System.err.println("Files generated successfully");
 
         } catch (IOException e) {
-            System.err.println("Error al generar archivos: " + e.getMessage());
+            System.err.println("Error generating files: " + e.getMessage());
         }
 
     }
@@ -41,9 +55,9 @@ public class GenerateInfoFiles {
             String header = id + ";" + name;
             writer.write(header + System.lineSeparator());
 
-            // Products
+            // Sales
             for (int i = 0; i < randomSalesCount; i++) {
-                // generate random sales
+                // Generate a random sale
                 String sale = generateRandomSale();
                 writer.write(sale + System.lineSeparator());
             }
@@ -52,18 +66,57 @@ public class GenerateInfoFiles {
 
     private static String generateRandomSale() {
 
-        // Structure sale returned: IDProducto1;CantidadProducto1Vendido;
+        // Structure sales returned: IDProducto1;CantidadProducto1Vendido;
         int sale_id = getRandomNumber(100);
         int sale_sum = getRandomNumber(10);
         return sale_id + ";" + sale_sum;
     }
 
-    public static boolean createProductsFile(int productsCount) {
-        return true;
+    public static void createProductsFile(int productsCount) throws IOException {
+        String fileName = "products.txt";
+        fileName = "files/" + fileName;
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            // Products
+            for (int i = 0; i < productsCount; i++) {
+                // Generate random sales
+                String product = generateRandomProduct();
+                writer.write(product + System.lineSeparator());
+            }
+        }
     }
 
-    public static boolean createSalesManInfoFile(int salesmanCount) {
-        return true;
+    private static String generateRandomProduct() {
+        // Structure product returned:
+        // IDProducto1;NombreProducto1;PrecioPorUnidadProducto1
+        int id_product = getRandomNumber(100);
+        String name_product = PRODUCTS[getRandomNumber(PRODUCTS.length) - 1];
+        int price_product = getRandomNumber(50000);
+        return id_product + ";" + name_product + ";" + price_product;
+    }
+
+    public static void createSalesManInfoFile(int salesmanCount) throws IOException {
+        String fileName = "vendors.txt";
+        fileName = "files/" + fileName;
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            // Vendors
+            for (int i = 0; i < salesmanCount; i++) {
+                // Generate random salesMans
+                String product = generateRandomSalesMan();
+                writer.write(product + System.lineSeparator());
+            }
+        }
+    }
+
+    private static String generateRandomSalesMan() {
+        // Structure salesman returned: TipoDocumento;NúmeroDocumento;NombresVendedor1;ApellidosVendedor1;
+        String document_type = "1"; 
+        int document_number = getRandomNumber(1000000000);
+        String vendor_name = NAMES[getRandomNumber(NAMES.length) - 1];
+        String vendor_lastname = SURNAMES[getRandomNumber(SURNAMES.length) - 1];
+
+        return document_type + ";" + document_number + ";" + vendor_name + ";" + vendor_lastname;
     }
 
     private static int getRandomNumber(int max_number) {
